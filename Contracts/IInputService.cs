02dc4f15c07c72a90bdee7c420667834d5d44a56
scotthtaylor1 <scotthtaylor@yahoo.com>
2019-01-18 15:06:18 -0700

    Add files via upload

--- Contracts/IInputService.cs
+++ Contracts/IInputService.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Contracts
 {
     public interface IInputService
     {
         void ReadDataIn(string[] args);
+        List<string> ReadFileData(string fileName);
+        List<string> ReadUserData();
     }
 }
